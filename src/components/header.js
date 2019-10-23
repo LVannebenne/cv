9e71ab603a8932bff64dcc5bc5f20ee8e460f4a9
LVannebenne <lindsay.vannebenne@gmail.com>
2019-10-23 20:56:24 +0200

    mobile version

--- src/components/header.js
+++ src/components/header.js
@@ -7,24 +7,26 @@ import {data} from "../data.json";
 export default function header() {
     return (
         <div className={"nav"}>
-            <div>
-                <img
-                    className={"profilePic"}
-                    src={Face}
-                    alt={"Profile portrait"}
-                />
+            <div className={"flex-row"}>
+                <div>
+                    <img
+                        className={"profilePic"}
+                        src={Face}
+                        alt={"Profile portrait"}
+                    />
+                </div>
+                <div>
+                    <h1 className={"title"}>
+                        <Link to={"/"}>{"Junior Web Developper"}</Link>
+                    </h1>
+                    <h5>
+                        {data.sentences.map(sentence => (
+                            <span key={sentence}>{sentence}</span>
+                        ))}
+                    </h5>
+                </div>
             </div>
-            <div>
-                <h1 className={"title"}>
-                    <Link to={"/"}>{"Junior Web Developper"}</Link>
-                </h1>
-                <h5>
-                    {data.sentences.map(sentence => (
-                        <span key={sentence}>{sentence}</span>
-                    ))}
-                </h5>
-                <Navbar />
-            </div>
+            <Navbar />
         </div>
     );
 }
